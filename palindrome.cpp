253e2b887a3ad77677a2011067d720df37c50b37
U-BSD\398256 <398256@SHS-DGDRCS2.beaverton.k12.or.us>
2022-09-19 14:05:29 -0700

    Finished project

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,3 +1,7 @@
+// This program takes in a string from a user and checks to see if it is a palindrome.
+// Author: Paige Wiley
+// Date: 9/19/2022
+
 #include <iostream>
 #include <cstring>
 #include <cctype>
@@ -7,24 +11,25 @@ using namespace std;
 int main(){
   char str[81];
   char str2[81];
+  char strP[81];
   int count = 0;
+  int index = 0;
   int nullIndex = 81;
-  cin >> str; //read in the string
-  cin.get(str, 81);
-  cout << "str:" << str << endl;
+ 
+  cin.get(str, 81); //get the string from user
+
 
    //find null index
    for(int i = 0; i < 81; i++){
      if(str[i] == '\0'){ //end of typed string
-       nullIndex = i;
+       nullIndex = i + 1;
+       break;
      }
    }
-   cout << "string 1:" << str << endl;
+   
    //convert to all lowercase
    for (int i = 0; i < nullIndex; i++){ 
      str[i] = tolower(str[i]);
-
-     cout << str[i] << endl;
     
     if((str[i] >= 97 && str[i] <= 122) ||
         (str[i] >= 48 && str[i] <= 57)){ // char is a lowercase letter or number  
@@ -33,32 +38,30 @@ int main(){
       count++;
     } else if (str[i] == '\0'){ //add a null value to the new string
       str2[count] = '\0';
+      count--;
     }
-
-    //cout << str[i] << endl;
-   //get ride of all repeats in string...
-
-   
   }
  
+      
+  index = count;
     
+  //copy string onto new string backwards
+  for(int i = 0; i <= index; i++){
+    strP[i] = str2[count];
+    count--;
+  }
   
-  // strncpy(str, str2, 81);
-
-  cout << "string 1:" << str << endl;
-  cout << "string2:" << str2 << endl;
+  cout << "String: " << str2 << endl;
+  cout << "String backwards: " << strP << endl;
   
+  //compare strings:
+  if(strcmp(strP, str2) == 0){ //equal
+    cout << "Palindrome!" << endl;
+  }else{ //not equal
+    cout << "Not Palindrome :(" << endl;
+  }
   
-  
-  
-  
-
-  
-  
-  
-
-  
-
-  //cout << str2 << endl; //print str2
+ 
+    
   return 0;
 }
